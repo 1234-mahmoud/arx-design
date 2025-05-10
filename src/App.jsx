6fbd872dea2f95fd8167d20bbdbaace727090145
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-05-11 01:46:54 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className='App'>
-      {/* <Slider/> */}
+      <Slider/>
       <About_us/>
       <Shorts/>
       <Services/>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,42 +1,59 @@
-import { AiOutlineSearch } from "react-icons/ai"; 
-import { BiFilterAlt } from "react-icons/bi"; 
+import { useState, useEffect } from "react";
 import React from "react";
 import "../style/Slider.css";
-import home_img from "../assets/home_img.png";
+import home_img1 from "../assets/home_img.png";
+import home_img2 from "../assets/slide_2.jpg";
+import home_img3 from "../assets/slide_3.png";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
 export default function Slider() {
+  const pics = [home_img1, home_img2, home_img3];
+
+  const [count, setCount] = useState(0);
+
+
   return (
-    <div className="slider">
+    <div className="parent">
+      <div className="slider-wrapper">
+          {
+            pics.map((i,index)=>(
+              <div className="card-img"
+           key={index}
+                style={{
+                  zIndex: index === count ? pics.length : pics.length - index, // Selected image highest zIndex
+                  opacity: index === count ? 1 : 0.3,
+                  transition: "opacity 0.5s ease, z-index 0.8s ease-in-out",
+                  position: "absolute",
+                }}
 
-      <img src={home_img} alt="" />
-      <div className="title">
-        <h1>NEW</h1>
-        <h1>ADMINISTRATIVE</h1>
-        <h1>CAPITAL</h1>
+          >
+            <img src={i} alt="" />
+          </div>
+            ))
+          }
+  
       </div>
-      <div className="description">
-        <p>
-          New Administrative Capital - Downtown Plot NO. MU5-39, Area 3275m²."
-          (Commercial - Medical - Offices) Consists Of 15 Mixed-Used Floors.
-        </p>
-      </div>
-      <div className="search">
-        <input type="search" name="" id="" placeholder="Search About Projects"/>
-        <div className="search_box_icons">
-            <div className="filter_icon">
-                <BiFilterAlt />
-            </div>
-            <div className="search_icon">
-                <AiOutlineSearch />
-            </div>
+        {/* Pagination with original pics length */}
+        <div className="pagination">
+          {Array.from({ length: pics.length }).map((_, i) => (
+            <span
+              key={i}
+              className={
+                i === count % pics.length ? "active" : "" // Active bullet based on original pics length
+              }
+              onClick={() => setCount(i)}
+            >
+               <img src={pics[i]} alt="" />
+
+               <div className="circles"></div>
+               <div className="circles"></div>
+               <div className="circles"></div>
+               <div className="circles"></div>
+               <div className="circles"></div>
+            </span>
+          ))}
         </div>
-      </div>
-
-      {/* --------------slider pagination-------------- */}
-
-      <div className="pagination">
-        
-      </div>
-
     </div>
   );
 }
