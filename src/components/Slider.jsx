bf2e4a3f22ef170fd42d7b462d87a31a0eef7b11
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-05-13 03:09:25 +0300

    up

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -8,54 +8,83 @@ import home_img3 from "../assets/slide_3.png";
 import { css } from "@emotion/react";
 
 export default function Slider() {
-  const pics = [home_img1, home_img2, home_img3];
+  const slide_data = [
+    {
+      img: home_img1,
+      title: "AURA MALL",
+      description: `  AURA MALL In the most distinguished areas 
+      of New Damietta, in the center of 
+      the second district, plot No. 10, near Al-Saidi Street,`,
+    },
+    {
+      img: home_img2,
+      title: "Kentro Tower",
+      description: `New Administrative Capital - Downtown Plot NO. MU5-39, 
+                Area 3275m²." 
+                (Commercial - Medical - Offices) Consists Of 15 Mixed-Used Floors. `,
+    },
+    {
+      img: home_img3,
+      title: "UNI 1",
+      description: `UNI Project Series A Group Of UNI Projects 
+      Has Been Implemented, Which Adds a New and 
+      Distinctive Look To The Real Estate Development Market. `,
+    },
+  ];
 
   const [count, setCount] = useState(0);
-
 
   return (
     <div className="parent">
       <div className="slider-wrapper">
-          {
-            pics.map((i,index)=>(
-              <div className="card-img"
-           key={index}
-                style={{
-                  zIndex: index === count ? pics.length : pics.length - index, // Selected image highest zIndex
-                  opacity: index === count ? 1 : 0.3,
-                  transition: "opacity 0.5s ease, z-index 0.8s ease-in-out",
-                  position: "absolute",
-                }}
-
+        {slide_data.map((i, index) => (
+          <div
+            className="card-img"
+            key={index}
+            style={{
+              zIndex:
+                index === count ? slide_data.length : slide_data.length - index, // Selected image highest zIndex
+              opacity: index === count ? 1 : 0.3,
+              transition: "opacity 0.5s ease, z-index 0.8s ease-in-out",
+              position: "absolute",
+            }}
           >
-            <img src={i} alt="" />
+            <img src={i.img} alt="" />
+
+            <p className="slide_txt">New Administrative Capital</p>
           </div>
-            ))
-          }
-  
+        ))}
       </div>
-        {/* Pagination with original pics length */}
-        <div className="pagination">
-          {Array.from({ length: pics.length }).map((_, i) => (
+      <div className="pagination">
+        {slide_data.map((slide, i) => (
+          <div>
             <span
               key={i}
               className={
-                i === count % pics.length ? "active" : "" // Active bullet based on original pics length
+                i === count % slide_data.length ? "active" : "" // Active bullet based on original slide_data length
               }
               onClick={() => setCount(i)}
             >
-               <img src={pics[i]} alt="" />
+              <img src={slide.img} alt="" />
 
-               <div className="circles"></div>
-               <div className="circles"></div>
-               <div className="circles"></div>
-               <div className="circles"></div>
-               <div className="circles"></div>
+              <div className="circle">
+                <div className="circles"></div>
+                <div className="circles"></div>
+                <div className="circles"></div>
+                <div className="circles"></div>
+                <div className="circles"></div>
+              </div>
 
-               <div className="animated_line"></div>
+              <div className="slide-info">
+                <h1>{slide.title}</h1>
+                <p>{slide.description}</p>
+              </div>
+
+              {<div className="animated_line"></div>}
             </span>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
