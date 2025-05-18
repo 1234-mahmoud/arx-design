29402e285d50fa9b835c4527c4e22eab46e8a3d8
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-05-19 02:37:09 +0300

    up

---
+++ src/components/Location.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import pic from "../assets/AdminCap.jpg";
+import "../style/Locations.css";
+export default function Location() {
+  return (
+    <div className="proj-locations">
+      <div className="admin-cap">
+        <img src={pic} alt="" />
+      </div>
+      <div className="prime-location">
+       <div className="prime-location-container">
+         <div className="prime-location-info-1">
+          <h2>Prime Location</h2>
+          <p>
+            Strategically situated in Downtown, the heart of the New
+            Administrative Capital, KÈNTRO TOWER enjoys an exceptional position
+            surrounded by key landmarks.
+          </p>
+        </div>
+
+        <div className="prime-location-info-2">
+          <p>Getting There</p>
+          <p>
+            With seamless access from major roads and proximity to 80% of the
+            capital’s iconic destinations, KÈNTRO TOWER stands as a central hub
+            in the New Capital. Minutes away from the Gold Market, Mosque of
+            Egypt, monorail station, and the Green River, its location ensures
+            unparalleled connectivity and convenience.
+          </p>
+        </div>
+        <button>get directions</button>
+       </div>
+      </div>
+    </div>
+  );
+}
--- src/components/OneProject.jsx
+++ src/components/OneProject.jsx
@@ -3,12 +3,16 @@ import '../style/OneProject.css'
 import SliderProjects from './SliderProjects'
 import ProjInfo from './ProjInfo'
 import BuildingInfo from './BuildingInfo'
+import Location from './Location'
+import Footer from './Footer';
 export default function OneProject() {
   return (
     <div className='one-proj'>
         <SliderProjects/>
         <ProjInfo/>
         <BuildingInfo/>
+        <Location/>
+        <Footer show={true}/>
     </div>
   )
 }
