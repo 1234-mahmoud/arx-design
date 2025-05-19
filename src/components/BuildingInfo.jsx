3aea7e84cf42e877ed4546e6a050b3116406d0a7
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-05-19 03:01:40 +0300

    up

--- src/components/BuildingInfo.jsx
+++ src/components/BuildingInfo.jsx
@@ -24,6 +24,7 @@ export default function BuildingInfo() {
             onClick={() => setActiveTab({ ...activeTab, box1: "features" })}
             className={activeTab.box1 === "features" ? "active" : ""}
           >
+            <span class="material-symbols-outlined">video_library</span>
             Features
           </button>
 
@@ -31,6 +32,7 @@ export default function BuildingInfo() {
             onClick={() => setActiveTab({ ...activeTab, box1: "photos" })}
             className={activeTab.box1 === "photos" ? "active" : ""}
           >
+            <span class="material-symbols-outlined">image</span>
             Photos
           </button>
 
@@ -38,6 +40,7 @@ export default function BuildingInfo() {
             onClick={() => setActiveTab({ ...activeTab, box1: "videos" })}
             className={activeTab.box1 === "videos" ? "active" : ""}
           >
+            <span class="material-symbols-outlined">video_library</span>
             Videos
           </button>
 
@@ -45,6 +48,7 @@ export default function BuildingInfo() {
             onClick={() => setActiveTab({ ...activeTab, box1: "plan" })}
             className={activeTab.box1 === "plan" ? "active" : ""}
           >
+            <span class="material-symbols-outlined">home_work</span>
             Plan
           </button>
         </div>
@@ -83,19 +87,31 @@ export default function BuildingInfo() {
 
         <div className="specifications">
           <div className="space">
-            <span className="specifications-name">space</span>
+            <span className="specifications-name">
+              <span class="material-symbols-outlined">zoom_in_map</span>
+              space
+            </span>
             <span className="specifications-val">1200 sqm</span>
           </div>
           <div className="floors">
-            <span className="specifications-name">floors</span>
+            <span className="specifications-name">
+              <span class="material-symbols-outlined">stacks</span>
+              floors
+            </span>
             <span className="specifications-val">8</span>
           </div>
           <div className="rooms">
-            <span className="specifications-name">rooms</span>
+            <span className="specifications-name">
+              <span class="material-symbols-outlined">house</span>
+              rooms
+            </span>
             <span className="specifications-val">100</span>
           </div>
           <div className="type">
-            <span className="specifications-name">type</span>
+            <span className="specifications-name">
+              <span class="material-symbols-outlined">house</span>
+              type
+            </span>
             <span className="specifications-val">commercial</span>
           </div>
         </div>
